435a5b1eec8713270e7dadd2bddf4d0e03726a59
artostapyshyn <artostapyshyn@gmail.com>
2024-11-05 20:42:25 +0200

    port update

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/persons', personsRouter);
 app.use('/categories', categoriesRouter);
 app.use('/themes', themesRouter);
 
-const port = 4000 || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
